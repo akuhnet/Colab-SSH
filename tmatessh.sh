a17dcf62374b6c632ef401432f5066b016bf1ac5
akuh <81416541+akuhnet@users.noreply.github.com>
2022-02-11 12:47:12 +0700

    Create tmatessh.sh

---
+++ tmatessh.sh
@@ -0,0 +1,11 @@
+pkill -9 tmate
+wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready &> /dev/null
+echo "==============="
+echo "==============="
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "#{tmate_ssh}"
+echo "==============="
+echo "==============="
+sleep 9999999999
